ef6e3890d5d9f7a43c0c0fd336b7c404cf647179
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2023-12-05 09:16:25 +0300

    İsimlendirmeler yapıldı.

--- PurchasingProject.WpfUi/HomeWindow.xaml.vb
+++ PurchasingProject.WpfUi/HomeWindow.xaml.vb
@@ -1,9 +1,16 @@
 ﻿Public Class HomeWindow
     Private ReadOnly _userItem As UserItem
+    Private ReadOnly _httpApiService As IHttpApiService
 
     Public Sub New(userItem As UserItem)
         InitializeComponent()
         _userItem = userItem
+    End Sub
+
+    Public Sub New(userItem As UserItem, httpApiService As IHttpApiService)
+        InitializeComponent()
+        _userItem = userItem
+        _httpApiService = _httpApiService
     End Sub
 
     Private Sub hamburgerMenu_Loaded(sender As Object, e As RoutedEventArgs)
@@ -12,21 +19,42 @@
         If accountType = 1 Then
 
         ElseIf accountType = 2 Then
-
+            companyButton.Visibility = Visibility.Collapsed
         Else
-
+            companyButton.Visibility = Visibility.Collapsed
+            departmentButton.Visibility = Visibility.Collapsed
+            personsButton.Visibility = Visibility.Collapsed
+            categoryButton.Visibility = Visibility.Collapsed
+            currencyButton.Visibility = Visibility.Collapsed
+            supplierButton.Visibility = Visibility.Collapsed
         End If
     End Sub
 
-    Private Sub minimize_Click(sender As Object, e As RoutedEventArgs)
+    Private Sub homeButton_Click(sender As Object, e As RoutedEventArgs)
 
     End Sub
 
-    Private Sub maximize_Click(sender As Object, e As RoutedEventArgs)
+    Private Sub companyButton_Click(sender As Object, e As RoutedEventArgs)
 
     End Sub
 
-    Private Sub close_Click(sender As Object, e As RoutedEventArgs)
-        Application.Current.Shutdown()
+    Private Sub departmentButton_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub personsButton_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub categoryButton_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub currencyButton_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub supplierButton_Click(sender As Object, e As RoutedEventArgs)
+
     End Sub
 End Class
